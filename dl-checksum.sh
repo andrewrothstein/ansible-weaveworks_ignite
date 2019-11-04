4fdbe11fccdb476f239a072235d065148db607f0
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-11-03 22:23:12 -0500

    refactor to include ignited too

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,22 +1,40 @@
 #!/usr/bin/env sh
-VER=v0.4.1
 DIR=~/Downloads
-MIRROR=https://github.com/weaveworks/ignite/releases/download/$VER
+MIRROR=https://github.com/weaveworks/ignite/releases/download
 
 dl()
 {
-    APP=$1
-    RFILE=$APP$SUFFIX
-    URL=$MIRROR/$RFILE
-    LFILE=$DIR/$RFILE-$VER
+    local app=$1
+    local ver=$2
+    local arch=$3
+    local rfile=$app-$arch
+    local url=$MIRROR/$ver/$rfile
+    local lfile=$DIR/$rfile-$ver
 
-    if [ ! -e $LFILE ];
+    if [ ! -e $lfile ];
     then
-        wget -q -O $LFILE $URL
+        wget -q -O $lfile $url
     fi
 
-    printf "    # %s\n" $URL
-    printf "    %s: sha256:%s\n" $VER `sha256sum $LFILE | awk '{print $1}'`
+    printf "      # %s\n" $url
+    printf "      %s: sha256:%s\n" $arch `sha256sum $lfile | awk '{print $1}'`
 }
 
-dl ignite
+dl_ver() {
+    local app=$1
+    local ver=$2
+    printf "    %s:\n" $ver
+    dl $app $ver amd64
+    dl $app $ver arm64
+}
+
+VERS=${1:-v0.5.0 v0.5.1 v0.5.2 v0.5.3 v0.5.4 v0.5.5 v0.6.0 v0.6.1 v0.6.2}
+printf "weaveworks_ignite_checksums:\n"
+printf "  ignite:\n"
+for ver in $VERS; do
+    dl_ver ignite $ver
+done
+printf "  ignited:\n"
+for ver in $VERS; do
+    dl_ver ignited $ver
+done
